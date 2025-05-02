6806508d9504ff1f1a3d7789c52cceceeebf30d5
ykozhan <justerkanat@gmail.com>
2025-05-03 02:10:39 +0500

    feat: api rickmorty

--- internal/infrastructure/api/rickandmorty.go
+++ internal/infrastructure/api/rickandmorty.go
@@ -1,7 +1,16 @@
 package api
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"1337b04rd/pkg/logger"
+)
+
 type CharacterResponse struct {
-	ID    int    `json:"id"`
 	Name  string `json:"name"`
 	Image string `json:"image"`
 }
@@ -10,39 +19,35 @@ type RickAndMortyAPI struct {
 	client *http.Client
 }
 
-func NewRickAndMortyAPI() *RickAndMortyAPI {
+func NewRickAndMortyAPI(l *logger.CustomLogger) *RickAndMortyAPI {
 	return &RickAndMortyAPI{
 		client: &http.Client{Timeout: 10 * time.Second},
 	}
 }
 
-baseURL := "https://rickandmortyapi.com/api"
+var baseURL = "https://rickandmortyapi.com/api"
 
-
-func (c *RickAndMortyAPI) GetRandomCharacter(id int)  (avatarURL string, name string, err error){
-	url := fmt.Sprintf("%s/character/%d", baseURL)
-	resp, err := c.httpClient.Get(url)
+func (c *RickAndMortyAPI) GetRandomCharacter(id int) (avatarURL string, name string, err error) {
+	url := fmt.Sprintf("%s/character/%d", baseURL, id)
+	resp, err := c.client.Get(url)
 	if err != nil {
-		logger.Error("failed to send GET request to Rick and Morty API", "url", url, "error", err)
-		return nil, fmt.Errorf("rickmorty GET error: %w", err)
+		return "", "", fmt.Errorf("RickMorty GET error: %w", err)
 	}
 	defer resp.Body.Close()
 
+	// на случай если рик и морти не робит или инет дерьмо
 	if resp.StatusCode != http.StatusOK {
-		logger.Error("rickmorty API returned non-200 status", "url", url, "status", resp.StatusCode)
-		return nil, fmt.Errorf("rickmorty API status: %d", resp.StatusCode)
+		return "", "", fmt.Errorf("RickMorty API status: %d", resp.StatusCode)
 	}
 
 	var data CharacterResponse
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		logger.Error("failed to decode response from Rick and Morty API", "url", url, "error", err)
-		return nil, fmt.Errorf("decode error: %w", err)
+		return "", "", fmt.Errorf("decode error: %w", err)
 	}
 
 	if data.Image == "" || data.Name == "" {
-		logger.Error("rickmorty API returned character with missing fields", "id", id)
-		return nil, errors.New("character missing name or image")
+		return "", "", errors.New("character missing name or image")
 	}
 
-	return &data, nil
+	return data.Image, data.Name, nil
 }
